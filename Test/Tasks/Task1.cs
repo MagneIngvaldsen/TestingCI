7a40b91aea9e08f0eb2a06b0ecec09c801787f04
Magne Ingvaldsen <magne.ingvaldsen@gmail.com>
2016-05-02 00:45:23 +0200

    Added sln with test

---
+++ Test/Tasks/Task1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    public static class Task1
+    {
+        public static bool AlwaysTrue()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Test/Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tasks;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(Task1.AlwaysTrue());
+        }
+    }
+}
